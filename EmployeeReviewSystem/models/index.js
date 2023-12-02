24d2bb42e0a5679ac02c10db54779d99e541fa8c
Siddhant Chauhan <91963573+SiddChauhan19@users.noreply.github.com>
2023-12-02 14:16:53 +0530

    Delete EmployeeReviewSystem directory

--- EmployeeReviewSystem/models/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.User = require('./User')
-module.exports.Feedback = require('./Feedback')
-module.exports.Performance = require('./Performance')
-module.exports.Request = require('./Request')
